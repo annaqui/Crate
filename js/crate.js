7f09ad94a0a155bc37328802bb62e75a544d3ed1
Andrew Aitken <andrew@thescorpion.co.uk>
2015-05-19 16:13:33 +0100

    Updating Crate.js to make the values update
    Using the val() method of the control to update the value, this doesn't
    seem to update the actual value attribute of the input tag so might not
    really work??

--- js/crate.js
+++ js/crate.js
@@ -1,12 +1,41 @@
-var counter = 0;
-    
-        $(document).ready(function() {
+	var minimum = 0;
+	var maximum = 100;
+ 
+
+	$(document).ready(function() {
     
             $("#plus").click(function(){
-                counter++;
-                console.log(counter);
-    
-                $("#total").text(counter);
+				input = $("#total")
+				value = parseInt(input.val());
+	
+				//If the value isn't a number of exceeds the maximum value just set it to the max
+				if( isNaN(value) || value >= maximum){
+					input.val(maximum);
+				} else {
+					input.val(value + 1);
+				}
             });
     
+            $("#minus").click(function(){
+				input = $("#total")
+				value = parseInt(input.val());
+	
+				//Assuming they're not sending you beer they should not go beleow the minimum
+				if( isNaN(value) || value <= minimum){
+					input.val(minimum);
+				} else {
+					input.val(value - 1);
+				}
+            });
+			
+			$("#total").change(function() {
+				input = $("#total")
+				value = parseInt(input.val());
+				
+				//Maybe this should be minimum instead?
+				if(isNaN(value)){
+					input.val(maximum);
+				}
+			});
+
         });
